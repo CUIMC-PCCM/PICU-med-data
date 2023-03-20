217eb5c9dc834a601323d387af2d9eae746c266f
moe1619 <joshua.motelow@gmail.com>
2023-03-20 13:27:44 -0400

    Update med-data.sh
    - added setup_repo

--- Scripts/med-data.sh
+++ Scripts/med-data.sh
@@ -1,1 +1,15 @@
+#\!/bin/bash
+# 2023-03-13
+# med management
+
+
+########## set up directory structure
+if [[ $1 = "setup_repo" ]]
+then
+  mkdir Input
+  mkdir Intermediate
+  mkdir Output
+  exit
+fi
+
 

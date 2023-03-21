c897f44abc1dffc7faec49d9cf2a89fde52b7edd
moe1619 <joshua.motelow@gmail.com>
2023-03-21 13:35:10 -0400

    Update deidentify_data.R

--- Scripts/deidentify_data.R
+++ Scripts/deidentify_data.R
@@ -18,23 +18,26 @@ library(tidyverse)
 library(data.table)
 library(here)
 library(docopt)
+"%!in%" <- Negate("%in%")
 arguments <- docopt(doc, version = 'deidentify_data.R')
 
 if(arguments$debug == "TRUE"){
   arguments<-list()
   arguments$path_to_data <- here("Input","RITM0429582_V1_epicVisitAdtMar_halfRows.txt")
-  arguments$identity_header
+  arguments$path_to_key_data <- here("Input","identify_key.csv")
+  arguments$identity_header <- "EMPI"
+  arguments$project_name <- "PGX"
 }
 
 #' Initialize log file
 #' @param time_case_prefix time stamp and case group combo to append to beginning of logfile
 #' @param function_name name of function being logged
 initialize_logfile<- function(time_case_prefix, function_name){
+  dir.create(here("Intermediate"))
+  dir.create(here("Intermediate","Logs"))
+  dir.create(here("Intermediate","Logs",function_name))
   tryCatch(
     {
-      dir.create(here("Intermediate"))
-      dir.create(here("Intermediate","Logs"))
-      dir.create(here("Intermediate","Logs",function_name))
       logr::log_open(here("Intermediate","Logs",function_name, paste0(time_case_prefix,function_name,"_logfile.log")))
       return(TRUE)
     },
@@ -42,11 +45,11 @@ initialize_logfile<- function(time_case_prefix, function_name){
       message('An Error Occurred in creating the logfile')
       print(e)
       stop("error")
+      return(NA)
     },
     warning=function(w) {
       message('A Warning Occurred in creating the logfile')
       print(w)
-      return(NA)
     }
   )
 }
@@ -72,3 +75,78 @@ tryCatch(
   }
 )
 
+tryCatch(
+  {
+    key_data <- fread(arguments$path_to_key_data)
+  }, 
+  error=function(e){
+    message("error loading in key data")
+    logr::log_print(e)
+  },
+  warning=function(w) {
+    message('A Warning Occurred loading in key data')
+    logr::log_print(w)
+    return(NA)
+  }
+)
+
+# remote duplicate keys
+tryCatch(
+  {
+    duplicate_keys <-  key_data$identified_key[duplicated(key_data$identified_key)]
+    location_unique_keys <- as.character(identified_data[[c(arguments$identity_header)]]) %!in% as.character(duplicate_keys)
+    identified_data_unique <- identified_data[location_unique_keys , ]
+    nrow(identified_data)
+    nrow(identified_data_unique)
+  }, 
+  error=function(e){
+    message("error creating deidentified key")
+    logr::log_print(e)
+  },
+  warning=function(w) {
+    message('A Warning occurred creating deidentified key')
+    logr::log_print(w)
+    return(NA)
+  }
+)
+
+tryCatch(
+  {
+    merge_df <- merge(identified_data_unique,key_data, by.x = arguments$identity_header, by.y = "identified_key", all.x = TRUE)
+    # identified_data$deidentified_id <- sapply(identified_data[[c(arguments$identity_header)]], function(x) key_data$deidentified_key[key_data$identified_key == x])
+  }, 
+  error=function(e){
+    message("error creating deidentified key")
+    logr::log_print(e)
+  },
+  warning=function(w) {
+    message('A Warning occurred creating deidentified key')
+    logr::log_print(w)
+    return(NA)
+  }
+)
+
+
+tryCatch(
+  {
+    new_names <- names(merge_df)
+    new_names <- new_names[new_names != arguments$identity_header]
+    write_df = subset(merge_df, select = c(new_names) )
+    write.csv(x = write_df, file = gzfile(here("Intermediate","deidentified_data.csv.gz")), quote = FALSE, row.names = FALSE, col.names = TRUE)
+    # write.csv(x = write_df, file = here("Intermediate","deidentified_data.csv"), quote = FALSE, row.names = FALSE, col.names = TRUE)
+  }, 
+  error=function(e){
+    message("error writing deidentified data")
+    logr::log_print(e)
+  }
+  # ,
+  # warning=function(w) {
+  #   message('A Warning occurred writing deidentified data')
+  #   logr::log_print(w)
+  #   return(NA)
+  # }
+)
+
+
+logr::log_print("Finished")
+logr::log_close()

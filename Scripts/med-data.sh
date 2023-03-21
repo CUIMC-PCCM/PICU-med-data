56f7fe66e87d2c32ff9612d34cb9bbeee4cf57b1
moe1619 <joshua.motelow@gmail.com>
2023-03-21 15:33:36 -0400

    Update med-data.sh

--- Scripts/med-data.sh
+++ Scripts/med-data.sh
@@ -13,11 +13,19 @@ then
 fi
 
 
+project_name=$(cat ./Input/med-data.yaml | shyaml get-value INPUT_VARIABLE.project_name)
+echo "project name is $project_name"
 ########## deidentify med data
-if [[ $1 = "setup_repo" ]]
+if [[ $1 = "deidentify_data" ]]
 then
+  project_name=$(cat ./Input/med-data.yaml | shyaml get-value INPUT_VARIABLE.project_name)
+  identity_header=$(cat ./Input/med-data.yaml | shyaml get-value INPUT_VARIABLE.identified_data_column)
+  path_to_key_data=$(cat ./Input/med-data.yaml | shyaml get-value INPUT_VARIABLE.identified_data_key)
 
-	Rscript
+
+	Rscript ./Scripts/deidentify_data.R --path_to_data $identified_data  --identity_header $identify_header --path_to_key_data path_to_key_data --project_name $project_name
 
 fi
+
+
 

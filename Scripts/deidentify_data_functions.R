1d1065ed3953e16b1042ca0c3921c9e330a622e1
moe1619 <joshua.motelow@gmail.com>
2023-09-15 09:26:40 -0400

    Update deidentify_data_functions.R
    - adding function for for outputing mrn set membership between MRN and key
    - adding functino for writing deidentified data

--- Scripts/deidentify_data_functions.R
+++ Scripts/deidentify_data_functions.R
@@ -8,3 +8,21 @@ return_room_location <- function(i,data_df, loc_df){
     # quit("no", status = 10)
   })
 }
+
+check_mar_vs_identity_key <- function(mar_df, key_df){
+  logr::log_print("in MAR and key")
+  logr::log_print(length(mrn_in_key_and_mar <- unique(intersect(key_df$identified_key_char,mar_df$EMPI_char))))
+  logr::log_print("in MAR but not in key")
+  logr::log_print(length(MAR_mrn_wo_deidentified_key <- unique(mar_df$EMPI_char[mar_df$EMPI_char %!in% key_df$identified_key_char ])))
+  logr::log_print("in key but not in mrn")
+  logr::log_print(length(deidentified_key_mrn_wo_MAR <- unique(key_df$identified_key_char[key_df$identified_key_char %!in%  mar_df$EMPI_char])))
+}
+
+writing_deidentified_data <- function(merge_df, cdw_epic){
+  new_names <- names(merge_df)
+  new_names <- new_names[new_names != arguments$identity_header]
+  write_df = subset(merge_df, select = c(new_names) )
+  write_df$EMPI_char <- NULL
+  write_df$identified_key <- NULL
+  write.table(x = write_df %>% filter(!is.na(deidentified_key)), file = gzfile(here("Intermediate",paste0(time_case_prefix,cdw_epic,"_deidentified_data.tsv.gz"))), quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+}
